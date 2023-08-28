f05665aa7aab2348334fda7eacd2392e5bb1ec76
赵永盛 <1562743134@qq.com>
2023-08-28 13:53:05 +0800

    feat: 冒泡排序

---
+++ src/冒泡排序.ts
@@ -0,0 +1,19 @@
+const arr = [1,3,4,2];
+
+const sort = (arr: number[]) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+  
+      if (arr[i] > arr[j]) {
+        const t = arr[i];
+        
+        arr[i] = arr[j];
+        arr[j] = t;
+      }
+    }
+  }
+}
+
+sort(arr);
+
+console.log(arr);
