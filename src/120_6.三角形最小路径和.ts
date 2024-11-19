115a4df9b7ac9928486a9775c3fb7d2500ec3083
赵永盛 <1562743134@qq.com>
2024-11-19 13:53:28 +0800

    feat: 一波

--- src/120_6.三角形最小路径和.ts
+++ src/120_6.三角形最小路径和.ts
@@ -2,6 +2,9 @@
 // 动规
 function minimumTotal(triangle: number[][]): number {
   // 行数
+  // 不同点：倒序遍历三角形行
+  // 题目要求是路径和最小，举的例子也是自顶向下，但是自底向上也一样能求出最小路径和（非递归），只要从底部向上走，路径和最小就可以了
+  // 自底向上好处：求某点的 (i,j) 路径和时，下一行的 (i+1, j), (i+1, j+1) 一定存在，每行的第一个节点、最后一个节点 不用再单独判断&处理
   const n = triangle.length;
 
   // 计算下一行的值时，dp 还保持上一行的值
@@ -9,14 +12,16 @@ function minimumTotal(triangle: number[][]): number {
   // 总结：后面元素的新值依赖前面元素的旧值，需要从后往前计算
   const dp: number[] = new Array(n + 1).fill(0);
 
-  // 不同点：倒序遍历三角形行
-  // todo ?
+  // 从最后一一行（底部）开始遍历
   for (let i = n - 1; i >= 0; i--) {
+    // 遍历列
     for (let j = 0; j <= i; j++) {
+      // 每行的 j 列，依赖下一行的 j, j + 1 列
       dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j];
     };
   };
 
+  // dp[0] 存放的值，就是最后走到顶点的值
   return dp[0];
 };
 
