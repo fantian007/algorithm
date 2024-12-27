40d02d97b8b9a1cdb1b5909ab70bc3debbfa8139
赵永盛 <1562743134@qq.com>
2024-12-27 13:46:40 +0800

    feat: 一波

--- src/134_2.加油站.ts
+++ src/134_2.加油站.ts
@@ -6,7 +6,7 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
   // 到达下一个站点的油箱剩余油量
   let sum = 0;
   // 最小剩余油量
-  let minSum = sum;
+  let minSum = 0;
   // 起始点
   let start = 0;
 
@@ -14,9 +14,15 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
     // 以 i 为起点，到达了 i + 1 的油箱剩余油量
     sum += gas[i] - cost[i];
 
+    // 能走到这里的，都是 < 0 的值
     if (sum < minSum) {
       minSum = sum;
-      start = i + 1; // i+1 位置是图中最低点
+      /**
+       * i + 1 位置是图中最低点
+       * 
+       * 如果 i+1 超出了下标范围，代表最后一个站点回到索引0站点出现了缺油，意思跑了一圈出现了缺油，那么会走 sum < 0 的逻辑，最终返回 -1
+       */
+      start = i + 1;
     }
   };
 
@@ -28,8 +34,9 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
   return start;
 }
 
+const r = canCompleteCircuit([2,2,0], [2,3,1]); // 3
 // const r = canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]); // 3
-const r = canCompleteCircuit([3, 1, 1], [1, 2, 2]); // 0
+// const r = canCompleteCircuit([3, 1, 1], [1, 2, 2]); // 0
 // const r = canCompleteCircuit([2, 3, 4], [3, 4, 3]);
 // const r = canCompleteCircuit([10, 5, 2, 3, 5], [50, 2, 1, 10, 2]);
 
