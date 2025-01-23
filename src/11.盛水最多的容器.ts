a9c45f2511cfa56c9066cef4749a92d68b43e8e0
赵永盛 <1562743134@qq.com>
2025-01-23 13:07:11 +0800

    feat: 一波

---
+++ src/11.盛水最多的容器.ts
@@ -0,0 +1,35 @@
+/**
+ * 双指针
+ * @see https://leetcode.cn/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
+ * 思路：每次移动时，移动短柱子，储水量才可能变大
+ * 解释：
+ * 1. 固定左右柱子时，储水量由短柱子决定。
+ * 2. 移动高的柱子（向对方移动），宽度变小，移动之后高度可能增加/减小
+ *    1. 如果增加，储水量不变，因为短柱子决定水量
+ *    2. 如果减少，储水量更少，因为宽度和高度都小
+ * 所以，移动高的柱子，储水量不会变多，只可能变少
+ * 3. 移动短的柱子（向对方移动），按同样的道理推理，宽度变小，但是高度可能增加，储水量是可能变大的
+ */
+function maxArea(height: number[]): number {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+};
+
+const r = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]); // 49
+// const r = maxArea([1, 1]); // 1
+console.log(r);
+
+export { }
--- src/15.三数之和.ts
+++ src/15.三数之和.ts
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode.cn id=15 lang=typescript
- *
- * [15] 三数之和
- */
-
-// @lc code=start
 function threeSum(nums: number[]): number[][] {
   const res = [];
   const n = nums.length;
@@ -12,8 +5,8 @@ function threeSum(nums: number[]): number[][] {
 
   // 提取 2sum 公共方法
   const twoSum = (nums: number[], start: number, target: number) => {
-    
-    const ret: number[][] =[];
+
+    const ret: number[][] = [];
     let lo = start;
     let hi = nums.length - 1;
 
@@ -23,16 +16,13 @@ function threeSum(nums: number[]): number[][] {
       let right = nums[hi];
 
       if (sum < target) {
-        while (lo < hi && left === nums[lo]) {
-          lo++;
-        }
+        lo++;
       } else if (sum > target) {
-        while (lo < hi && right === nums[hi]) {
-          hi--;
-        }
+        hi--;
       } else {
         ret.push([nums[lo], nums[hi]]);
 
+        // 相同的元素跳过
         while (lo < hi && left === nums[lo]) {
           lo++;
         }
@@ -45,20 +35,48 @@ function threeSum(nums: number[]): number[][] {
     return ret;
   }
 
-  for(let i = 0; i < n; i++) {
+  // 遍历数组，选出一个数作为第一个数
+  for (let i = 0; i < n; i++) {
+    // 排序之后，当前项 >0，那么后面的数都大于 0，不用看了
+    if (nums[i] > 0) {
+      break;
+    }
+
+    // 连续相同的数，取第一个，其它的跳过
+    // 如果当前的数和上一个数相同（上一个数已 push)，那么跳过当前数
+    // while (i > 0 && nums[i] === nums[i - 1]) {
+    //   i++;
+    // }
+
+    // 从 (i, len) 从找 其余 2 个数
     const r = twoSum(nums, i + 1, 0 - nums[i]);
 
-    for(let j = 0; j < r.length; j++) {
+    for (let j = 0; j < r.length; j++) {
       res.push([nums[i], ...r[j]]);
     };
 
-    // 排除第一个元素重复的情况
-    while (i < n -1 && nums[i] === nums[i + 1]) {
+    // 连续相同的数，取第一个，其它的跳过
+    // 如果下一个数和当前数相同（当前数已 push)，那么跳过下一个数
+    while (i < n - 1 && nums[i] === nums[i + 1]) {
       i++;
     }
   };
 
   return res;
 };
-// @lc code=end
 
+/**
+ * 连续相同的数，一定要取第一个，例如：[-4, -1, -1, 0, 1, 2]
+ * 取第一个 -1：剩余2数组合 [[-1, 2], [0, 1]]
+ * 取第二个 -1: 剩余2数组合：[[0, 1]]，是第一个数的子集
+ * 取第一个数，能获取到所有组合；取后面的数，组合数减少，且子集组合是重复的，所以需要跳过
+ */
+
+// const r = threeSum([-1, 0, 1, 2, -1, -4]); // [[-1,-1,2],[-1,0,1]]
+// const r = threeSum([-3, -3, 0, 1, 2, 3, 6]); // [[-3,-3,6],[-3,0,3],[-3,1,2]]
+// const r = threeSum([-2, 0, 0, 2, 2]); // [[-2,0,2],[-2,0,2]]
+const r = threeSum([1, -1, -1, 0]); // [[-1,0,1]]
+
+console.log(JSON.stringify(r));
+
+export { };
