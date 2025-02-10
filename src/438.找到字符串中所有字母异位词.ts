efb2ed948c19ea1866c47179adf9e621a619a3fa
赵永盛 <1562743134@qq.com>
2025-02-10 13:51:18 +0800

    feat: 一波

--- src/438.找到字符串中所有字母异位词.ts
+++ src/438.找到字符串中所有字母异位词.ts
@@ -1,25 +1,23 @@
-/*
- * @lc app=leetcode.cn id=438 lang=typescript
- *
- * [438] 找到字符串中所有字母异位词
- */
-
-// @lc code=start
+// 滑动窗口
 function findAnagrams(s: string, p: string): number[] {
-  // 异位词，就是排列啊
-  const res: number[] = [];
+  const r: number[] = [];
 
+  // p 中各字符的数量
+  const needs = {};
+  // 窗口中各字符数量（只记录 p 中的字符)
+  const win = {};
+  // 窗口中达到 needs 中各字符数量的 key 的个数
+  let matchKeysLen = 0;
+  // 窗口左右指针
   let left = 0;
   let right = 0;
-  let valid = 0;
-  const needs = {};
-  const window = {};
 
-  for (let i = 0; i < p.length; i++) {
-    const char = p.charAt(i);
-
-    (needs[char] ? needs[char]++ : needs[char] = 1);
+  // 收集 p 中字符数量
+  for (const c of p) {
+    needs[c] ??= 0;
+    needs[c]++;
   }
+  // p 中字符的个数
   const keysLen = Object.keys(needs).length;
 
   while (right < s.length) {
@@ -27,39 +25,48 @@ function findAnagrams(s: string, p: string): number[] {
 
     right++;
 
+    // win 只记录 需要的字符
     if (needs[rv]) {
-      (window[rv] ? window[rv]++ : window[rv] = 1);
+      win[rv] ??= 0;
+      win[rv]++;
 
-      if (window[rv] === needs[rv]) {
-        valid++;
+      // 如果某个字符达到了 needs 中相同字符的 个数，使 matchKeysLen 数量 +1
+      if (win[rv] === needs[rv]) {
+        matchKeysLen++;
       }
     }
 
-    while (valid === keysLen) {
+    // 如果窗口中各字符的数量都满足了 needs 中要求的数量
+    while (matchKeysLen === keysLen) {
+      // 由于 win 只记录了需要的字符，截取的字符串中间可能会有其它字符，这种是不符合的，要去掉。通过判断截取字符串的长度来判断此类场景。
       if (right - left === p.length) {
-        res.push(left);
+        r.push(left);
       }
 
+      // 先取值再自增
       const lv = s[left];
 
       left++;
 
+      // 只关注需要的字符
       if (needs[lv]) {
-        if (needs[lv] === window[lv]) {
-          valid--;
-          window[lv]--;
+        if (needs[lv] === win[lv]) {
+          // 左侧指针右移，窗口字符减少
+          win[lv]--;
+          // 窗口中关注字符的数量变少，不满足数量，所以 matchKeysLen 要减1
+          matchKeysLen--;
         } else {
-          window[lv]--;
-          
-          if (window[lv] === 0) {
-            valid--;
-          }
+          win[lv]--;
         }
       }
     }
   }
 
-  return res;
+  return r;
 };
-// @lc code=end
 
+// const r = findAnagrams('cbaebabacd', 'abc'); // [0, 6]
+const r = findAnagrams('abacbabc', 'abc'); // [1,2,3,5]
+console.log(r);
+
+export {}
