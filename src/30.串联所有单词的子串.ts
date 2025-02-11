e949ea04d928489f6b2a4dc7d73a9d921f23fc3e
赵永盛 <1562743134@qq.com>
2025-02-11 13:37:02 +0800

    feat: 一波

---
+++ src/30.串联所有单词的子串.ts
@@ -0,0 +1,10 @@
+// todo
+function findSubstring(s: string, words: string[]): number[] {
+  
+  return [];
+};
+
+const r = findSubstring('', []);
+console.log(r);
+
+export {}
--- src/76.最小覆盖子串.ts
+++ src/76.最小覆盖子串.ts
@@ -1,66 +1,56 @@
-/*
- * @lc app=leetcode.cn id=76 lang=typescript
- *
- * [76] 最小覆盖子串
+/**
+ * @see [438.找到字符串中所有字母异位词](./438.找到字符串中所有字母异位词.ts)
  */
-
-// @lc code=start
 function minWindow(s: string, t: string): string {
   let left = 0;
   let right = 0;
-  let valid = 0;
+  let matchKeysLen = 0;
   let start = 0;
   let len = Number.MAX_SAFE_INTEGER;
 
-  const window = {};
+  const win = {};
   const needs = {};
 
-  for (let i = 0; i < t.length; i++) {
-    const char = t.charAt(i);
-
-    (needs[char] ? needs[char]++ : needs[char] = 1);
+  for (const c of t) {
+    needs[c] ??= 0;
+    needs[c]++;
   }
-
   const needsKeysCnt = Object.keys(needs).length;
 
   while (right < s.length) {
-
     /**
      * 先取值，后 ++ 相当于 [left, right) 左闭右开
      */
-    const rv = s.charAt(right);
+    const rv = s[right];
     
     right++;
 
     if (needs[rv]) {
-      (window[rv] ? window[rv]++ : window[rv] = 1);
+      win[rv] ??= 0;
+      win[rv]++;
 
-      if (window[rv] === needs[rv]) {
-        valid++;
+      if (win[rv] === needs[rv]) {
+        matchKeysLen++;
       }
     }
 
-    while (valid === needsKeysCnt) {
+    while (matchKeysLen === needsKeysCnt) {
       // 记录窗口最小宽度
       if (right - left < len) {
         start = left;
         len = right - left;
       }
 
-      const lv = s.charAt(left);
+      const lv = s[left];
 
       left++;
 
       if (needs[lv]) {
-        if (window[lv] === needs[lv]) {
-          window[lv]--;
-          valid--;
+        if (win[lv] === needs[lv]) {
+          win[lv]--;
+          matchKeysLen--;
         } else {
-          window[lv]--;
-
-          if (window[lv] === 0) {
-            valid--;
-          }
+          win[lv]--;
         }
       }
     }
@@ -68,5 +58,4 @@ function minWindow(s: string, t: string): string {
 
   return len === Number.MAX_SAFE_INTEGER ? '' : s.substr(start, len);
 };
-// @lc code=end
 
