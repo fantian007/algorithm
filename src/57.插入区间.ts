b29a964eec847c5fae8c999040e69be7f2fe226d
赵永盛 <1562743134@qq.com>
2025-03-19 13:51:06 +0800

    feat: 一波

--- src/57.插入区间.ts
+++ src/57.插入区间.ts
@@ -9,8 +9,7 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
   intervals.sort((a, b) => a[0] - b[0]);
 
   const r: number[][] = [];
-  let start = intervals[0][0];
-  let end = intervals[0][1];
+  let [start, end] = intervals[0];
 
   for (let i = 1; i < intervals.length; i++) {
     if (intervals[i][0] <= end) {
