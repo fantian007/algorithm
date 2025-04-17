2d7cf75bd3922e73a3ab4dc774f1b75290ad7a2f
赵永盛 <1562743134@qq.com>
2025-04-17 13:47:30 +0800

    feat: 一波

--- src/83.删除排序链表中的重复元素.ts
+++ src/83.删除排序链表中的重复元素.ts
@@ -1,24 +1,6 @@
-/*
- * @lc app=leetcode.cn id=83 lang=typescript
- *
- * [83] 删除排序链表中的重复元素
- */
-
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import { ListNode } from "./ListNode";
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-
   if (head === null) {
     return null;
   }
@@ -42,5 +24,5 @@ function deleteDuplicates(head: ListNode | null): ListNode | null {
 
   return head;
 };
-// @lc code=end
 
+export {}
